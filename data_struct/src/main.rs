c04aa5238033e122213b0c575385d54d81c087b9
junlin.li <junlin.li@xiaoyangedu.com>
2022-07-20 23:44:02 +0800

    add 语句与表达式、函数

--- data_struct/src/main.rs
+++ data_struct/src/main.rs
@@ -1,3 +1,5 @@
+mod expression;
+
 use num::complex::Complex;
 fn main() {
     // 8 位	    i8	u8
@@ -95,4 +97,6 @@ fn main() {
     // 不能说 main 函数无返回值，因为没有返回值的函数在 Rust 中是有单独的定义的：发散函数( diverge function )，顾名思义，无法收敛的函数
     // 常见的 println!() 的返回值也是单元类型 ()。
     // 再比如，你可以用 () 作为 map 的值，表示我们不关注具体的值，只关注 key。 这种用法和 Go 语言的 struct{} 类似，可以作为一个值用来占位，但是完全不占用任何内存
+
+    expression::expression();
 }
